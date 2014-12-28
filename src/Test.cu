83eda3133817f685b6c1cac5e99d1cdb3506fd70
Michael Matheny <michaelmatheny2.71@gmail.com>
2014-12-28 05:19:58 -0500

    Removed the Testing file since it is misleading

--- src/Test.cu
+++
@@ -1,9 +0,0 @@
-#include "Vecky.hpp"
-using namespace Vecky;
-
-int main(int argc, char* argv[]){
-  VecN<float, 3> vec1;
-  VecN<float, 3> vec2;
-  VecN<float, 3> vec3;
-  return (vec1 + 3.f * vec2 + vec3).get<0>();
-}
