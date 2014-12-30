3669d165966ce74e67ba314796ec571d4a92d4cf
Michael Matheny <michaelmatheny2.71@gmail.com>
2014-12-29 19:22:38 -0500

    Fixed some stuff. Cross product is broken right now.

--- src/Vecky.hpp
+++ src/Vecky.hpp
@@ -45,6 +45,37 @@ public:
   }
 };
 
+  
+template <typename OP, typename E2, typename T, unsigned int N>
+struct ExprNode <T, OP, E2, T, N> {
+  T const& _e1;
+  E2 const& _e2;
+public:
+  __host__ __device__ inline ExprNode(T const& e1, E2 const& e2)
+    : _e1(e1), _e2(e2)
+  {}
+  
+  template<unsigned int I>
+  __host__ __device__ inline T get() const {
+    return OP::template apply<T, N, I>(_e1, _e2.template get<I>());
+  }
+};
+
+template <typename E1, typename OP, typename T, unsigned int N>
+struct ExprNode <E1, OP, T, T, N> {
+  E1 const& _e1;
+  T const& _e2;
+public:
+  __host__ __device__ inline ExprNode(E1 const& e1, T const& e2)
+    : _e1(e1), _e2(e2)
+  {}
+  
+  template<unsigned int I>
+  __host__ __device__ inline T get() const {
+    return OP::template apply<T, N, I>(_e1.template get<I>(), _e2);
+  }
+};
+  
 template <typename T>
 struct ScalarNode {
   T const& _v;
@@ -61,7 +92,8 @@ public:
 
 template<typename T, typename Opr, unsigned int N>
 struct Binary_Map {
-  void apply(T& dst, T const& src){
+  __host__ __device__ static void 
+  apply(T& dst, T const& src){
     dst[N] = Opr::template apply<T, 0, 0>(dst.template get<N>(), src.template get<N>());
     Binary_Map<T, Opr, N - 1>::apply(dst, src);
   }
@@ -69,14 +101,15 @@ struct Binary_Map {
 
 template<typename T, typename Opr>
 struct Binary_Map <T, Opr, 0> {
-  void apply(T& dst, T const& src){
+  __host__ __device__ inline static void
+  apply(T& dst, T const& src){
     dst[0] = Opr::template apply<T, 0, 0>(dst.template get<0>(), src.template get<0>());
   }
 };
 
 template<typename E1, typename OP, typename E2, typename T, unsigned int N, unsigned int i>
 struct Eval {
-  __host__ __device__ inline void
+  __host__ __device__ inline static void
   apply(T* _data, ExprNode<E1, OP, E2, T, N> const& expr){
     _data[i] = expr.template get<i>();
     Eval<E1, OP, E2, T, N, i + 1>::apply(_data, expr);
@@ -85,7 +118,7 @@ struct Eval {
 
 template<typename E1, typename OP, typename E2, typename T, unsigned int N>
 struct Eval<E1, OP, E2, T, N, N> {
-  __host__ __device__ inline void
+  __host__ __device__ inline static void
   apply(T* _data, ExprNode<E1, OP, E2, T, N> const& expr){}
 };
 
@@ -94,6 +127,7 @@ template <typename T, unsigned int N>
 class VecN {
   T _data[N];
 public:
+
   
   template<typename E1, typename OP, typename E2>
   __host__ __device__ inline
@@ -103,6 +137,13 @@ public:
 
   __host__ __device__ inline
   VecN(){}
+
+  __host__ __device__ inline
+  VecN(T const& val){
+    for (unsigned int i = 0; i < N; i++){
+      _data[i] = val;
+    }
+  }
 
   __host__ __device__ inline T& operator[](unsigned int i){
     return _data[i];
@@ -184,7 +225,7 @@ BINARY_FUNCTION_STRUCT(VecExponent, pow)
 #define BINARY_VECTOR_OPERATION(OP_NAME, SYMBOL, RET_T)			\
 template<typename E1, typename E2, typename E3, typename E4, typename PrevOp1, typename PrevOp2, typename T, unsigned int N> \
 __host__ __device__ inline ExprNode<ExprNode<E1, PrevOp1, E2, T, N>, OP_NAME, ExprNode<E3, PrevOp2, E4, T, N>, RET_T, N> const \
-operator SYMBOL (ExprNode<E1, PrevOp1, E2, T, N> const& expNode1, ExprNode<E2, PrevOp2, E3, T, N> const& expNode2){ \
+operator SYMBOL (ExprNode<E1, PrevOp1, E2, T, N> const& expNode1, ExprNode<E3, PrevOp2, E4, T, N> const& expNode2){ \
   return ExprNode<ExprNode<E1, PrevOp1, E2, T, N>, OP_NAME, ExprNode<E3, PrevOp2, E4, T, N>, RET_T, N>(expNode1, expNode2); \
 }   \
     \
@@ -221,31 +262,128 @@ BINARY_VECTOR_OPERATION(OprLEq, !=, bool)
 
 #define SCALAR_VECTOR_OPERATION(OP_NAME, SYMBOL)   \
 template<typename E1, typename E2, typename PrevOp, typename T, unsigned int N>	\
-__host__ __device__ inline  ExprNode<ExprNode<E1, PrevOp, E2, T, N>, OP_NAME, ScalarNode<T>, T, N> const \
+__host__ __device__ inline  ExprNode<ExprNode<E1, PrevOp, E2, T, N>, OP_NAME, T, T, N> const \
 operator SYMBOL (ExprNode<E1, PrevOp, E2, T, N> const& expNode, T const& val){	\
-  return ExprNode<ExprNode<E1, PrevOp, E2, T, N>, OP_NAME, ScalarNode<T>, T, N>(expNode, ScalarNode<T>(val)); \
+  return ExprNode<ExprNode<E1, PrevOp, E2, T, N>, OP_NAME, T, T, N>(expNode, val); \
 }    \
 template<typename E1, typename E2, typename PrevOp, typename T, unsigned int N>  \
-__host__ __device__ inline ExprNode<ScalarNode<T>, OP_NAME, ExprNode<E1, PrevOp, E2, T, N>, T, N> const \
+__host__ __device__ inline ExprNode<T, OP_NAME, ExprNode<E1, PrevOp, E2, T, N>, T, N> const \
 operator SYMBOL (T const& val, ExprNode<E1, PrevOp, E2, T, N> const& expNode){  \
-  return ExprNode<ScalarNode<T>, OP_NAME, ExprNode<E1, PrevOp, E2, T, N>, T, N>(ScalarNode<T>(val), expNode); \
+  return ExprNode<T, OP_NAME, ExprNode<E1, PrevOp, E2, T, N>, T, N>(val, expNode); \
 }  \
 template<typename T, unsigned int N>			\
-__host__ __device__ inline  ExprNode<VecN<T, N>, OP_NAME, ScalarNode<T>, T, N> const	\
+__host__ __device__ inline  ExprNode<VecN<T, N>, OP_NAME, T, T, N> const	\
 operator SYMBOL (VecN<T, N> const& vec, T const& val){	\
-  return ExprNode<VecN<T, N>, OP_NAME, ScalarNode<T>, T, N>(vec, ScalarNode<T>(val)); \
+  return ExprNode<VecN<T, N>, OP_NAME, T, T, N>(vec, val); \
 }    \
 template<typename T, unsigned int N>			\
-__host__ __device__ inline  ExprNode<ScalarNode<T>, OP_NAME, VecN<T, N>, T, N> const	\
+__host__ __device__ inline  ExprNode<T, OP_NAME, VecN<T, N>, T, N> const	\
 operator SYMBOL (T const& val, VecN<T, N> const& vec){		\
-  return ExprNode<ScalarNode<T>, OP_NAME, VecN<T, N>, T, N>(ScalarNode<T>(val), vec); \
+  return ExprNode<T, OP_NAME, VecN<T, N>, T, N>(val, vec); \
 } 
 
 SCALAR_VECTOR_OPERATION(OprMult, *)
 SCALAR_VECTOR_OPERATION(OprDiv, /)
-}
-/*
-  Functions that operate on two elements at the same time.
- */
 
+#define BINARY_VECTOR_FUNCTION(F_OP, F_NAME, RET_T)			\
+template<typename E1, typename E2, typename E3, typename E4, typename PrevOp1, typename PrevOp2, typename T, unsigned int N> \
+__host__ __device__ inline ExprNode<ExprNode<E1, PrevOp1, E2, T, N>, F_OP, ExprNode<E3, PrevOp2, E4, T, N>, RET_T, N> const \
+F_NAME(ExprNode<E1, PrevOp1, E2, T, N> const& expNode1, ExprNode<E3, PrevOp2, E4, T, N> const& expNode2){ \
+  return ExprNode<ExprNode<E1, PrevOp1, E2, T, N>, F_OP, ExprNode<E3, PrevOp2, E4, T, N>, RET_T, N>(expNode1, expNode2); \
+}   \
+    \
+template<typename E1, typename E2, typename PrevOp, typename T, unsigned int N>  \
+__host__ __device__ inline ExprNode<ExprNode<E1, PrevOp, E2, T, N>, F_OP, VecN<T, N>, RET_T, N> const   \
+F_NAME (ExprNode<E1, PrevOp, E2, T, N> const& expNode, VecN<T, N> const& vec){  \
+  return ExprNode<ExprNode<E1, PrevOp, E2, T, N>, F_OP, VecN<T, N>, RET_T, N>(expNode, vec);  \
+}  \
+  \
+template<typename E1, typename E2, typename PrevOp, typename T, unsigned int N>  \
+__host__ __device__ inline ExprNode<VecN<T, N>, F_OP, ExprNode<E1, PrevOp, E2, T, N>, RET_T, N> const  \
+F_NAME (VecN<T, N> const& vec, ExprNode<E1, PrevOp, E2, T, N> const& expNode){ \
+  return ExprNode<VecN<T, N>, F_OP, ExprNode<E1, PrevOp, E2, T, N>, RET_T, N>(vec, expNode);  \
+}  \
+  \
+template<typename T, unsigned int N>  \
+__host__ __device__ inline ExprNode<VecN<T, N>, F_OP, VecN<T, N>, RET_T, N> const  \
+F_NAME (VecN<T, N> const& vec1, VecN<T, N> const& vec2){   \
+  return ExprNode<VecN<T, N>, F_OP, VecN<T, N>, RET_T, N>(vec1, vec2);  \
+}
+
+struct CrossProd {  
+  template <typename T, unsigned int N, unsigned int I>
+  __host__ __device__ inline static T apply(T const& v1, T const& v2) {
+    return v1[(I + 1) % 3] * v2[(I + 2) % 3] - v1[(I + 2) % 3] * v2[(I + 1) % 3];
+  }
+};
+
+template<typename E1, typename E2, typename E3, typename E4, typename PrevOp1, typename PrevOp2, typename T, unsigned int 3> 
+__host__ __device__ inline Expr3ode<Expr3ode<E1, PrevOp1, E2, T, 3>, CrossProd, Expr3ode<E3, PrevOp2, E4, T, 3>, T, 3> const 
+cross(Expr3ode<E1, PrevOp1, E2, T, 3> const& exp3ode1, Expr3ode<E3, PrevOp2, E4, T, 3> const& exp3ode2){ 
+  return Expr3ode<Expr3ode<E1, PrevOp1, E2, T, 3>, CrossProd, Expr3ode<E3, PrevOp2, E4, T, 3>, T, 3>(exp3ode1, exp3ode2); 
+}   
+    
+template<typename E1, typename E2, typename PrevOp, typename T, unsigned int 3>  
+__host__ __device__ inline Expr3ode<Expr3ode<E1, PrevOp, E2, T, 3>, CrossProd, Vec3<T, 3>, T, 3> const   
+cross (Expr3ode<E1, PrevOp, E2, T, 3> const& exp3ode, Vec3<T, 3> const& vec){  
+  return Expr3ode<Expr3ode<E1, PrevOp, E2, T, 3>, CrossProd, Vec3<T, 3>, T, 3>(exp3ode, vec);  
+}  
+  
+template<typename E1, typename E2, typename PrevOp, typename T, unsigned int 3>  
+__host__ __device__ inline Expr3ode<Vec3<T, 3>, CrossProd, Expr3ode<E1, PrevOp, E2, T, 3>, T, 3> const  
+cross (Vec3<T, 3> const& vec, Expr3ode<E1, PrevOp, E2, T, 3> const& exp3ode){ 
+  return Expr3ode<Vec3<T, 3>, CrossProd, Expr3ode<E1, PrevOp, E2, T, 3>, T, 3>(vec, exp3ode);  
+}  
+  
+template<typename T, unsigned int 3>  
+__host__ __device__ inline Expr3ode<Vec3<T, 3>, CrossProd, Vec3<T, 3>, T, 3> const  
+cross (Vec3<T, 3> const& vec1, Vec3<T, 3> const& vec2){   
+  return Expr3ode<Vec3<T, 3>, CrossProd, Vec3<T, 3>, T, 3>(vec1, vec2);  
+}
+
+template<typename E1, typename E2, typename T, unsigned int from>
+struct DOT {
+  __host__ __device__ inline static T
+  apply(E1 const& e1, E2 const& e2) {
+    return e1.template get<from>() * e2.template get<from>() + DOT<E1, E2, T, from-1>::apply(e1, e2);
+  }
+};
+
+template<typename E1, typename E2, typename T>
+struct DOT<E1, E2, T, 0> {
+  __host__ __device__ inline static T
+  apply(E1 const& e1, E2 const& e2) {
+    return e1.template get<0>() * e2.template get<0>();
+  }
+};
+
+template<typename E1, typename E2, typename E3, typename E4, typename PrevOp1, typename PrevOp2, typename T, unsigned int N> 
+__host__ __device__ inline T const 
+dot (ExprNode<E1, PrevOp1, E2, T, N> const& expNode1, ExprNode<E3, PrevOp2, E4, T, N> const& expNode2){ 
+  return DOT<ExprNode<E1, PrevOp1, E2, T, N>, ExprNode<E3, PrevOp2, E4, T, N>, T, N-1>::apply(expNode1, expNode2); 
+}   
+    
+template<typename E1, typename E2, typename PrevOp, typename T, unsigned int N>  
+__host__ __device__ inline T const   
+dot (ExprNode<E1, PrevOp, E2, T, N> const& expNode, VecN<T, N> const& vec){  
+  return DOT<ExprNode<E1, PrevOp, E2, T, N>, VecN<T, N>, T, N-1>::apply(expNode, vec);  
+}  
+  
+template<typename E1, typename E2, typename PrevOp, typename T, unsigned int N>  
+__host__ __device__ inline T const  
+dot (VecN<T, N> const& vec, ExprNode<E1, PrevOp, E2, T, N> const& expNode){  
+  return DOT<VecN<T, N>, ExprNode<E1, PrevOp, E2, T, N>, T, N-1>::apply(vec, expNode);  
+}  
+  
+template<typename T, unsigned int N>  
+__host__ __device__ inline T const  
+dot (VecN<T, N> const& vec1, VecN<T, N> const& vec2){   
+  return DOT<VecN<T, N>, VecN<T, N>, T, N-1>::apply(vec1, vec2);
+}
+
+typedef VecN<Real_t, 4> Vec4;
+typedef VecN<Real_t, 3> Vec3;
+typedef VecN<Real_t, 2> Vec2;
+
+}
 #endif
